b1487452965fad9ab8257bd2cfcfdc91ef1bff9d
Brad Davis <bradsdavis@gmail.com>
2013-09-21 13:29:40 -0400

    Refactored module names.

--- model-x12/src/main/java/javax/edi/model/x12/segment/Detail.java
+++
@@ -1,9 +0,0 @@
-package javax.edi.model.x12.segment;
-
-import javax.edi.bind.annotations.EDISegmentGroup;
-
-@EDISegmentGroup
-public class Detail {
-
-	
-}
--- src/main/java/javax/edi/bind/EDIMessageException.java
+++
@@ -1,8 +0,0 @@
-package javax.edi.bind;
-
-public class EDIMessageException extends Exception {
-
-	public EDIMessageException(String message) {
-		super(message);
-	}
-}
